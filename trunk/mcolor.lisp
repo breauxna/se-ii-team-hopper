af9165f658ecaebdd5c437b0b535c44902e11fa1
thescienceofgod@gmail.com <thescienceofgod@gmail.com@13fc24a8-3cc5-b0b9-22c4-8bbc41515b0a>
2012-03-13 15:39:17 +0000

    Should be working now

--- trunk/mcolor.lisp
+++ trunk/mcolor.lisp
@@ -3,72 +3,90 @@
 ;; Leave these lines unchanged so that DrScheme can properly load this file.
 #reader(planet "reader.ss" ("cce" "dracula.plt") "modular" "lang")
 ;@author Youming Lin
-;@date Feb 20, 2012
+;@date Mar 13, 2012
 ;@version 1.0
 
 ;MColor module
 ;RGB->HSV and HSV->RGB conversion algorithms are from
-;http://www.cs.rit.edu/~ncs/color/t_convert.html
+;http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
 
 (require "specifications.lisp")
 
 (module MColor
-  (defun set-rgb (r g b)
-    (let* ((r2 (/ r 256))
-           (g2 (/ g 256))
-           (b2 (/ b 256))
-           (min (min (min r2 g2) b2))
-           (max (max (max r2 g2) b2))
-           (v (max))
-           (delta (- max min))
-           (s (if (equal max 0)
-                  0 ;s = 0, h is undefined
-                  (/ delta max)))
-           (h (if (equal max 0)
-                  -1
-                  (mod (+ 360
-                          (* 60 ;degrees
-                             (cond
-                               ((equal r2 max) (/ (- g2 b2) delta)) ;between yellow and magenta
-                               ((equal g2 max) (+ 2 (/ (- b2 r2) delta))) ;between cyan and yellow
-                               (t (+ 4 (/ (- r2 g2) delta)))))) 360)))) ;between magenta and cyan
-      (list r g b h s v)))
+  
+  (defun rgb? (r g b)
+    (and (natp r) (< r 256)
+         (natp g) (< g 256)
+         (natp b) (< b 256)))
+  
+  (defun hsv? (h s v)
+    (and (rationalp h) (>= h 0) (<= h 1)
+         (rationalp s) (>= s 0) (<= s 1)
+         (rationalp v) (>= v 0) (<= v 1)))
+  
+  ;red, green, blue are [0, 255]
+  (defun set-rgb (red green blue)
+    (if (rgb? red green blue)
+        (let* ((r (/ red 255))
+               (g (/ green 255))
+               (b (/ blue 255))
+               (minimum (min (min r g) b))
+               (maximum (max (max r g) b))
+               (v maximum)
+               (delta (- maximum minimum))
+               (s (if (equal maximum 0)
+                      0 ;s = 0, h is undefined
+                      (/ delta maximum)))
+               (h ( / (if (equal maximum minimum)
+                          0 ;achromatic
+                          (cond
+                            ((equal maximum r) (+ (/ (- g b) delta)
+                                                  (if (< g b)
+                                                      6
+                                                      0)))
+                            ((equal maximum g) (+ (/ (- b r) delta) 2))
+                            ((equal maximum b) (+ (/ (- r g) delta) 4))))
+                      6)))
+          (list red green blue h s v))
+        nil))
   
   (defun set-hsv (h s v)
-    (if (equal s 0)
-        (list v v v h s v) ;achromatic (grey)
-        (let* ((h2 (/ h 60)) ;sector 0 to 5
-               (i (floor h 1))
-               (f (- h i)) ;factorial part of h
+    (if (hsv? h s v)
+        (let* ((i (floor (* h 6) 1))
+               (f (- (* h 6) i))
                (p (* v (- 1 s)))
-               (q (* v (- 1 (* s f))))
-               (tt (* v (- 1 (* s (- 1 f))))))
+               (q (* v (- 1 (* f s))))
+               (u (* v (- 1 (* (- 1 f) s)))))
           (cond
-            ((equal i 0) (list (* v 256) (* tt 256) (* p 256) h s v))
-            ((equal i 1) (list (* q 256) (* v 256) (* p 256) h s v))
-            ((equal i 2) (list (* p 256) (* v 256) (* tt 256) h s v))
-            ((equal i 3) (list (* p 256) (* q 256) (* v 256) h s v))
-            ((equal i 4) (list (* t 256) (* p 256) (* v 256) h s v))
-            (t (list (* v 256) (* p 256) (* q 256) h s v))))))
+            ((equal (mod i 6) 0) (list (round (* v 255) 1) (round (* u 255) 1) (round (* p 255) 1) h s v))
+            ((equal (mod i 6) 1) (list (round (* q 255) 1) (round (* v 255) 1) (round (* p 255) 1) h s v))
+            ((equal (mod i 6) 2) (list (round (* p 255) 1) (round (* v 255) 1) (round (* u 255) 1) h s v))
+            ((equal (mod i 6) 3) (list (round (* p 255) 1) (round (* q 255) 1) (round (* v 255) 1) h s v))
+            ((equal (mod i 6) 4) (list (round (* u 255) 1) (round (* p 255) 1) (round (* v 255) 1) h s v))
+            ((equal (mod i 6) 5) (list (round (* v 255) 1) (round (* p 255) 1) (round (* q 255) 1) h s v))))
+        nil))
   
   (defun get-rgb (color)
     (mv-let (r g b h s v)
-            (color)
+            color
             (list r g b)))
   
   (defun get-hsv (color)
     (mv-let (r g b h s v)
-            (color)
+            color
             (list h s v)))
   
   (defun color? (color)
-    (mv-let (r g b h s v)
-            (color)
-            (and (natp r) (< r 256)
-                 (natp g) (< g 256)
-                 (natp b) (< b 256)
-                 (natp h) (< h 361)
-                 (>= s 0) (<= s 1)
-                 (>= v 0) (<= v 1))))
+    (if (equal (len color) 6)
+        (mv-let (r g b h s v)
+                color
+                (and (natp r) (< r 256)
+                     (natp g) (< g 256)
+                     (natp b) (< b 256)
+                     (rationalp h) (>= h 0) (<= h 1)
+                     (rationalp s) (>= s 0) (<= s 1)
+                     (rationalp v) (>= v 0) (<= v 1)
+                     (equal (set-rgb r g b) color)))
+        nil))
   
   (export IColor))
