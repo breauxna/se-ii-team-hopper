9082f4eaf15441303db03faf1a0cc28022269c09
thescienceofgod@gmail.com <thescienceofgod@gmail.com@13fc24a8-3cc5-b0b9-22c4-8bbc41515b0a>
2012-04-08 23:27:28 +0000

    Updated

--- trunk/main.lisp
+++ trunk/main.lisp
@@ -1,9 +1,9 @@
 ;; The first four lines of this file were added by Dracula.
 ;; They tell DrScheme that this is a Dracula Modular ACL2 program.
 ;; Leave these lines unchanged so that DrScheme can properly load this file.
-#reader(planet "reader.rkt" ("cce" "dracula.plt") "modular" "lang")
+#reader(planet "reader.ss" ("cce" "dracula.plt") "modular" "lang")
 ;@author Youming Lin
-;@date Apr 7, 2012
+;@date Apr 8, 2012
 ;@version 1.0
 
 (require "mmath.lisp")
@@ -14,10 +14,10 @@
 (require "minput.lisp")
 (require "mblur.lisp")
 (require "mborder.lisp")
+(require "mbrightness.lisp")
 (require "mcolormod.lisp")
 (require "mcontrast.lisp")
 (require "mcrop.lisp")
-(require "mbrightness.lisp")
 (require "mgreyscale.lisp")
 (require "mhistogram.lisp")
 (require "mhue.lisp")
@@ -31,25 +31,41 @@
 (require "msplitcolor.lisp")
 
 (module MMain
+  (include-book "io-utilities" :dir :teachpacks)
   (set-state-ok t)
   
-  (import IInput)
+  (import IBlur)
+  (import IBorder)
   (import IBrightness)
+  (import IColormod)
+  (import IContrast)
+  (import ICrop)
+  (import IGreyscale)
+  (import IHistogram)
+  (import IHue)
+  (import IInput)
+  (import IMerge)
+  (import IMirror)
   (import INegative)
-  (import IRotate)
-  (import ISplitcolor)
   (import IOperation)
   (import IOutput)
   (import IRead-Operations)
+  (import IResize)
+  (import IRotate)
+  (import ISaturation)
+  (import ISplitcolor)
+  
+;  (defun main (ops-file bmp-input bmp-output)
+;    (write-bmp-file (merge (read-bmp-file ops-file) (read-bmp-file bmp-input) 'up) bmp-output))
   
   (defun main (ops-file bmp-input bmp-output)
-    (write-bmp-file (brightness (read-bmp-file bmp-input) -.5) bmp-output))
+    (string-list->file bmp-output (histogram (read-bmp-file bmp-input)) state))
   
   (export IMain))
 
 (link Run (MMath MColor MImage MOperation MRead-Operations
-                  MInput MBlur MBorder MColormod MContrast
-                  MCrop MBrightness MGreyscale MHistogram MHue
+                  MInput MBlur MBorder MBrightness MColormod
+                  MContrast MCrop MGreyscale MHistogram MHue
                   MMerge MMirror MNegative MOutput MResize
                   MRotate MSaturation MSplitcolor MMain))
 (invoke Run)
