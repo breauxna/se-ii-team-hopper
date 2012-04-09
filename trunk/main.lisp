03519fe69676ae306ea732f666398d0f89762232
thescienceofgod@gmail.com <thescienceofgod@gmail.com@13fc24a8-3cc5-b0b9-22c4-8bbc41515b0a>
2012-04-09 20:50:01 +0000

    Updated blur execution

--- trunk/main.lisp
+++ trunk/main.lisp
@@ -2,11 +2,10 @@
 ;; They tell DrScheme that this is a Dracula Modular ACL2 program.
 ;; Leave these lines unchanged so that DrScheme can properly load this file.
 #reader(planet "reader.rkt" ("cce" "dracula.plt") "modular" "lang")
-;@author Michael Brandt
+;@author Youming Lin, Michael Brandt
 ;@date Apr 8, 2012
-;@version 1.1
+;@version 1.0
 
-(require "specifications.lisp")
 (require "mmath.lisp")
 (require "mcolor.lisp")
 (require "mimage.lisp")
@@ -15,10 +14,10 @@
 (require "minput.lisp")
 (require "mblur.lisp")
 (require "mborder.lisp")
+(require "mbrightness.lisp")
 (require "mcolormod.lisp")
 (require "mcontrast.lisp")
 (require "mcrop.lisp")
-(require "mbrightness.lisp")
 (require "mgreyscale.lisp")
 (require "mhistogram.lisp")
 (require "mhue.lisp")
@@ -32,6 +31,7 @@
 (require "msplitcolor.lisp")
 
 (module MMain
+  (include-book "io-utilities" :dir :teachpacks)
   (set-state-ok t)
   
   (import IInput)
@@ -58,39 +58,39 @@
     (if (listp operation)
         (let ((args (cadr operation))
               (op (car operation)))
-        (cond ((equal op 'crop)
-               (crop img (first args) (second args) (third args)
-                     (fourth args)))
-              ((equal op 'blur)
-               (blur img (first args) (second args) (third args)))
-              ((equal op 'merge)
-               (merge img (read-bmp-file (first args)) (second args)))
-              ((equal op 'border) 
-               (border img (first args) (second args)))
-              ((equal op 'negative)
-               (negative img))
-              ((equal op 'histogram) 
-               (histogram img))
-              ((equal op 'rotate)
-               (rotate img (first args)))
-              ((equal op 'resize) 
-               (resize-scale img (first args)))
-              ((equal op 'greyscale)
-               (greyscale img))
-              ((equal op 'saturation)
-               (saturation img (first args)))
-              ((equal op 'contrast)
-               (contrast img (first args)))
-              ((equal op 'splitcolor)
-               (splitcolor img (first args)))
-              ((equal op 'mirror)
-               (mirror img (first args)))
-              ((equal op 'hue)
-               (hue img (first args)))
-              ((equal op 'brightness)
-               (brightness img (first args)))
-              (t nil)))
-        nil)) 
+          (cond ((equal op 'crop)
+                 (crop img (first args) (second args) (third args)
+                       (fourth args)))
+                ((equal op 'blur)
+                 (blur img))
+                ((equal op 'merge)
+                 (merge img (read-bmp-file (first args)) (second args)))
+                ((equal op 'border) 
+                 (border img (first args) (second args)))
+                ((equal op 'negative)
+                 (negative img))
+                ((equal op 'histogram) 
+                 (histogram img))
+                ((equal op 'rotate)
+                 (rotate img (first args)))
+                ((equal op 'resize) 
+                 (resize-scale img (first args)))
+                ((equal op 'greyscale)
+                 (greyscale img))
+                ((equal op 'saturation)
+                 (saturation img (first args)))
+                ((equal op 'contrast)
+                 (contrast img (first args)))
+                ((equal op 'splitcolor)
+                 (splitcolor img (first args)))
+                ((equal op 'mirror)
+                 (mirror img (first args)))
+                ((equal op 'hue)
+                 (hue img (first args)))
+                ((equal op 'brightness)
+                 (brightness img (first args)))
+                (t nil)))
+        nil))
   
   (defun process-ops (ops img-in)
     (if (endp ops)
@@ -104,9 +104,10 @@
   
   (export IMain))
 
+
 (link Run (MMath MColor MImage MOperation MRead-Operations
-                 MInput MBlur MBorder MColormod MContrast
-                 MCrop MBrightness MGreyscale MHistogram MHue
+                 MInput MBlur MBorder MBrightness MColormod
+                 MContrast MCrop MGreyscale MHistogram MHue
                  MMerge MMirror MNegative MOutput MResize
                  MRotate MSaturation MSplitcolor MMain))
 (invoke Run)
