39142825afc9e2653cceabe7a67cbe0b86e353b1
breauxna@gmail.com <breauxna@gmail.com@13fc24a8-3cc5-b0b9-22c4-8bbc41515b0a>
2012-04-28 22:02:58 +0000

    Updated the specs file.

--- trunk/Modular Code/specifications.lisp
+++ trunk/Modular Code/specifications.lisp
@@ -2,69 +2,25 @@
 ;; They tell DrScheme that this is a Dracula Modular ACL2 program.
 ;; Leave these lines unchanged so that DrScheme can properly load this file.
 #reader(planet "reader.rkt" ("cce" "dracula.plt") "modular" "lang")
-;@author Youming Lin, Kyle Morse, Nathan Breaux
+;@author Nathan Breaux, Youming Lin, Kyle Morse
 ;@date Apr 8, 2012
 ;@version 1.0
 
 ;Specifications files
 ;Contains all of the interfaces for all of the modules
-(interface IError
-  
-  (sig make-error (msg))
-  
-  (sig error-p (x))
-  
-  (sig error-cons (x y)))
 
-(interface IExecute
-  
-  ; Evaluates a row based off of a predicate
-  (sig evaluate-filter (row predicate))
-  
-  ; Filters a list of rows into only those rows which match the filter
-  (sig filter-rows (rows filter))
-  
-  ; Extracts the data corresponding to a given field
-  (sig extract-field-data (field row))
-  
-  ; Reduces a row into values corresponding to a list of fields
-  ; '((field1 . value1) (field2 . value2) (field3 . value3))
-  ; -->
-  ; '(value1 value3)
-  (sig filter-fields-row (fields row))
-  
-  ; Filters multiple rows by a list of fields to display
-  (sig filter-fields-rows (fields rows))
-  
-  ; Evaluates a query on a list of rows
-  (sig rows-query->query-result (rows query))
-  
-  (sig row->csv-str (row))
-  
-  (sig rows->csv-strs (rows))
-  
-  (sig query-result->str (query-result))
-  
-  (sig rows-query-strs->query-result-str (data-str query-str))
-  
-  (sig queries->query-results-str (rows queries))
-  
-  (sig queries-data-str->query-results-str (data-str queries-str)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                      Main module                    ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(interface IList-Utilities
+(interface IRun
   
-  (sig booleanify (x))
-  
-  (sig first-n (n xs))
-  
-  ; Discards the first and last element of a list.
-  (sig sandwich-material (xs))
-  
-  (sig chunk-by-helper (delimiter xs so-far))
-  
-  (sig chunk-by (delimiter xs))
-  
-  (sig remove-nils (xs)))
+  (sig read-transform-write (data-in query-in f-out state))
+  (sig run (data-in query-in f-out)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                    Parsing modules                  ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (interface IParse-Data
   
@@ -124,10 +80,76 @@
   
   (sig str->queries (str)))
 
-(interface IRun
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                 Data structures module              ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(interface IStructures
+  (sig query (fields filter))
+  (sig query-fields (query))
+  (sig query-filter (query))
   
-  (sig read-transform-write (data-in query-in f-out state))
-  (sig run (data-in query-in f-out)))
+  (sig query-result (fields rows))
+  (sig query-result-fields (query-result))
+  (sig query-result-rows (query-result)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                   Execution module                  ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(interface IExecute
+  
+  ; Evaluates a row based off of a predicate
+  (sig evaluate-filter (row predicate))
+  
+  ; Filters a list of rows into only those rows which match the filter
+  (sig filter-rows (rows filter))
+  
+  ; Extracts the data corresponding to a given field
+  (sig extract-field-data (field row))
+  
+  ; Reduces a row into values corresponding to a list of fields
+  ; '((field1 . value1) (field2 . value2) (field3 . value3))
+  ; -->
+  ; '(value1 value3)
+  (sig filter-fields-row (fields row))
+  
+  ; Filters multiple rows by a list of fields to display
+  (sig filter-fields-rows (fields rows))
+  
+  ; Evaluates a query on a list of rows
+  (sig rows-query->query-result (rows query))
+  
+  (sig row->csv-str (row))
+  
+  (sig rows->csv-strs (rows))
+  
+  (sig query-result->str (query-result))
+  
+  (sig rows-query-strs->query-result-str (data-str query-str))
+  
+  (sig queries->query-results-str (rows queries))
+  
+  (sig queries-data-str->query-results-str (data-str queries-str)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                    Utility modules                  ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(interface IList-Utilities
+  
+  (sig booleanify (x))
+  
+  (sig first-n (n xs))
+  
+  ; Discards the first and last element of a list.
+  (sig sandwich-material (xs))
+  
+  (sig chunk-by-helper (delimiter xs so-far))
+  
+  (sig chunk-by (delimiter xs))
+  
+  (sig remove-nils (xs)))
 
 (interface IString-Utilities
   
@@ -145,14 +167,21 @@
   (sig break-by-line (str))
   )
 
-(interface IStructures
-  (sig query (fields filter))
-  (sig query-fields (query))
-  (sig query-filter (query))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                 Error checking module               ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(interface IError
   
-  (sig query-result (fields rows))
-  (sig query-result-fields (query-result))
-  (sig query-result-rows (query-result)))
+  (sig make-error (msg))
+  
+  (sig error-p (x))
+  
+  (sig error-cons (x y)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                  Improvements module                ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (interface IImprovements
   (sig new-is-string (token))
